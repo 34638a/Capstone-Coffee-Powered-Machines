1756df5cb0e08d2dcbb8153f962c33dfffeafcaa
Jordan Ramsay <jordanr123@bigpond.com>
2022-04-23 18:23:55 +1000

    Removed excess from main class

--- Backend/src/main/java/au/com/qut/cpm/capstone/CapstoneApplication.java
+++ Backend/src/main/java/au/com/qut/cpm/capstone/CapstoneApplication.java
@@ -1,13 +1,7 @@
 package au.com.qut.cpm.capstone;
 
-import au.com.qut.cpm.capstone.system.email.MailerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import javax.mail.MessagingException;
 
 @SpringBootApplication
 public class CapstoneApplication {
@@ -16,15 +10,4 @@ public class CapstoneApplication {
         SpringApplication.run(CapstoneApplication.class, args);
     }
 
-    @Autowired
-    private MailerService mailerService;
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void onStart() throws MessagingException {
-        System.out.println("Sending Mail Test");
-        mailerService.sendEmailTemplate("bradsterrpi@gmail.com", "Test Mail");
-        mailerService.sendEmail("bradsterrpi@gmail.com","This is not a template", "Not the Template");
-        System.out.println("Tests sent successfully!");
-        return;
-    }
 }
