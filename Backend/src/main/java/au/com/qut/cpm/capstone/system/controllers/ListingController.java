931c2f220389fc9e1210dd7f1e11fd73fd5f5030
Jordan Ramsay <jordanr123@bigpond.com>
2022-06-19 01:02:29 +1000

    Add listing endpoints for rss and atom feeds

--- Backend/src/main/java/au/com/qut/cpm/capstone/system/controllers/ListingController.java
+++ Backend/src/main/java/au/com/qut/cpm/capstone/system/controllers/ListingController.java
@@ -4,6 +4,9 @@ import au.com.qut.cpm.capstone.system.publication.Publication;
 import au.com.qut.cpm.capstone.system.publication.database.PublicationRepository;
 import au.com.qut.cpm.capstone.system.publication.publications.listing.Listing;
 import au.com.qut.cpm.capstone.system.publication.publications.listing.ListingNotFoundException;
+import com.rometools.rome.feed.atom.Content;
+import com.rometools.rome.feed.atom.Feed;
+import com.rometools.rome.feed.rss.Channel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,5 +43,33 @@ public class ListingController {
         }
         model.addAttribute("listing", listing);
         return "listings/listing";
+    }
+
+    @RequestMapping("listing/{id}/rss")
+    public Channel getListingRSSFeed(@PathVariable String id) {
+        Channel channel = new Channel();
+        channel.setFeedType("rss_2.0");
+        channel.setTitle("Mining Events Info Feed");
+        channel.setDescription("Articles posted to the listing feed.");
+        channel.setLink("https://www.miningevents.info");
+        channel.setUri("https://www.miningevents.info");
+        channel.setGenerator("In House Programming");
+        //Like more Entries here about different new topics
+        return channel;
+    }
+
+    @RequestMapping("listing/{id}/atom")
+    public Feed getListingAtomFeed(@PathVariable String id) {
+        Feed feed = new Feed();
+        feed.setFeedType("atom_1.0");
+        feed.setTitle("Mining Events Info Feed");
+        feed.setId("https://www.miningevents.info");
+
+        Content subtitle = new Content();
+        subtitle.setType("text/plain");
+        subtitle.setValue("Updates to the listing");
+        feed.setSubtitle(subtitle);
+
+        return feed;
     }
 }
